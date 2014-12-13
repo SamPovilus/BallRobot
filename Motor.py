89daf6e9270ffd30600aa86fda90ae2efc397e2e
spovilus <spovilus@lxlaptop>
2014-12-13 15:30:26 -0700

    moving motor to Motor, adding test

---
+++ Motor.py
@@ -0,0 +1,24 @@
+#import Adafruit_BBIO.PWM as motor
+import Queue
+from time import sleep
+import threading
+
+class Motor(threading.Thread):
+    
+    mySpeedsQueue = Queue.Queue(2)
+    def __init__(self, port, inverted,motorNumber,freq):
+        print "Motor thread started"
+        #motor.start(port,50,freq,inverted)
+        super(Motor, self).__init__()
+
+    def set_speed(self,speed):
+        self.mySpeedsQueue.put(speed)
+
+    def run(self):
+        if not self.mySpeedsQueue.empty():
+            print "Speed set to: " + self.mySpeedsQueue.get()
+            self.mySpeedsQueue.task_done()
+            
+        else:
+            print "Speed not changed"
+        sleep(0.5)
---
+++ motor_test.py
@@ -0,0 +1,14 @@
+import Motor
+from time import sleep
+
+myMotor = Motor.Motor(1,2,3,4)
+
+myMotor.start()
+
+i = 0 
+while 1:
+    print "Motor_test i:" + str(i)
+    myMotor.set_speed(i)
+    i = i + 1
+    sleep(1.4)
+    
