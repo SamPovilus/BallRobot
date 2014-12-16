bf1795c58a154189748559068bc34cca8633e6e1
spovilus <spovilus@lxlaptop>
2014-12-15 20:10:48 -0700

    adding comment as to location of transform matrix

--- TransformXYRotZToMotor.py
+++ TransformXYRotZToMotor.py
@@ -1,5 +1,6 @@
 import math
 
+#from http://cdn.intechopen.com/pdfs-wm/8875.pdf pg 221
 def TransformXYRotZToMotor(x,y,rotZ,d=1):
     m1 = (-math.sin(math.pi/3)*x+math.cos(math.pi/3)*y+d*rotZ)/3
     m2 = (                                      -1.0*y+     d)/3
