bc9de6df011660f2be6848e11e3d007ccc09fe67
spovilus <spovilus@lxlaptop>
2014-12-24 23:16:47 -0700

    adding telemetry in preperation for COSMOS

--- Motor.py
+++ Motor.py
@@ -4,6 +4,9 @@ import threading
 import collections
 import numpy
 import InvertPort
+import Queue
+import Struct
+
 
 class Motor(threading.Thread):
     pastSpeeds = None
@@ -12,23 +15,31 @@ class Motor(threading.Thread):
     myInvertPort = None
     myPWMPort = None
     myPeriod = None
-    myMotorDeadband = 80.0
+    myMotorDeadband = 0.0
     myDebug = None
-    def __init__(self, pwmPort, invertPortNumber, inverted,motorNumber,freq,period=0.02,filterDepth = 10, debug=False):
+    myTelemQueue = None
+    myNotificationQueue = None
+    def __init__(self, pwmPort, invertPortNumber, inverted, motorNumber, PWMFreq, telemQueue, notifcationQueue ,period=0.02,filterDepth = 10, debug=False):
         self.myMotorNumber = motorNumber
 	print "Motor " + str(self.myMotorNumber) + " thread started"
         self.myInvertPort = InvertPort.InvertPort(str(invertPortNumber))
         self.pastSpeeds =  collections.deque(maxlen=filterDepth)
         self.myPWMPort = pwmPort
-        PWMoutput.start(self.myPWMPort,50,freq,inverted)
+        PWMoutput.start(self.myPWMPort,50,PWMFreq,inverted)
         super(Motor, self).__init__()
-        self.daemon = True
+        self.daemon = True 
         self.myPeriod=period
         self.myDebug = debug
-
+        self.myTelemQueue = telemQueue
+        self.myNotificationQueue = notificationQueue
+        
     def set_speed(self,speed):
-        self.myDesiredSpeed = (speed*(100.0-self.myMotorDeadband))
-
+        #from open office f(x) =  - 58.2750582751x^4 + 221.8337218337x^3 - 321.3286713287x^2 + 258.7140637141x - 0.8391608392
+        #self.myDesiredSpeed = (speed*(100.0-self.myMotorDeadband))
+        self.myDesiredSpeed = -58.2750582751*(speed**4) + 221.8337218337*(speed**3) - 321.3286713287*(speed**2) + 258.7140637141*speed - 0.8391608392
+        self.myTelemQueue.put(Struct.pack('\f\f\l\l',self.myDesiredSpeed,float(speed),\xdeadbeef,\xdeadbeef))
+        self.myNotificationQueue.put(self.myMotorNumber+MOTOR_NOTIFICATION_OFFSET)
+        
     def run(self):
         while 1:
             #TODO: better filter design
--- motor_test.py
+++ motor_test.py
@@ -3,15 +3,24 @@ import ReadIMU
 import TransformXYRotZToMotor
 import Queue
 from time import sleep
+MOTOR_NOTIFICATION_OFFSET = 100
+IMU_NOTIFICATION_OFFSET = 200
+IMU_ID_ACC = 10
+IMU_ID_GYRO = 20
+Motor1TlmQueue = Queue.Queue(maxsize=10)
+Motor2TlmQueue = Queue.Queue(maxsize=10)
+Motor3TlmQueue = Queue.Queue(maxsize=10)
+IMUTlmQueue = Queue.Queue(maxsize=10)
+NotificationQueue = Queue.Queue(maxsize=4)
 
 XQueue = Queue.Queue(maxsize=2)
 YQueue = Queue.Queue(maxsize=2)
 
-myMotor0 = Motor.Motor("P9_14","P9_12",True,0,5000,period = 0.02,filterDepth = 2, debug= True)
-myMotor1 = Motor.Motor("P9_22","P9_18",True,1,5000,period = 0.02,filterDepth = 2, debug= True)
-myMotor2 = Motor.Motor("P8_13","P8_11",True,2,5000,period = 0.02,filterDepth = 2, debug= True)
+myMotor0 = Motor.Motor("P9_14","P9_12",True,0,5000,Motor1TlmQueue,NotificationQueue,period = 0.02,filterDepth = 2, debug= True)
+myMotor1 = Motor.Motor("P9_22","P9_18",True,1,5000,Motor3TlmQueue,NotificationQueue,period = 0.02,filterDepth = 2, debug= True)
+myMotor2 = Motor.Motor("P8_13","P8_11",True,2,5000,Motor3TlmQueue,NotificationQueue,period = 0.02,filterDepth = 2, debug= True)
 
-myIMU = ReadIMU.ReadIMU(0x53,"FAKE",XQueue,YQueue,period=0.02)
+myIMU = ReadIMU.ReadIMU(0x53,"FAKE",XQueue,YQueue,IMUTlmQueue,NotificationQueue,period=0.02)
 
 myMotor0.start()
 myMotor1.start()
