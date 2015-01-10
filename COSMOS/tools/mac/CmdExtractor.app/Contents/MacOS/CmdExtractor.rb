5d59e9fe674fc88293c424c3918845f6d7e198ff
spovilus <spovilus@lxlaptop>
2015-01-10 12:39:41 -0700

    updating

--- COSMOS/tools/mac/CmdExtractor.app/Contents/MacOS/CmdExtractor.rb
+++ COSMOS/tools/mac/CmdExtractor.app/Contents/MacOS/CmdExtractor.rb
@@ -1,21 +1,15 @@
 #!/usr/bin/env ruby
 # encoding: ascii-8bit
 
-# Ball Aerospace & Technologies Corp. Proprietary Information
+# Copyright 2014 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
 #
-# The information contained herein is the private property of Ball Aerospace &
-# Technologies Corp. that is being made available to the recipient under the
-# terms of a nondisclosure agreement or by other special arrangement.  It may
-# not be used, in whole or in part, except for the limited purpose for which it
-# has been furnished. It may not be distributed or reproduced, except as
-# specifically authorized by Ball Aerospace and with this legend conspicuously
-# attached.  This information is exempt from public disclosure under 5 U.S.C.
-# 552(b)(4), and its use by Government personnel is subject to the restrictions
-# imposed by 18 U.S.C. 1905.
-#
-# This source code module was developed with funds provided by
-# Capital funding under charge number 11418.13.001.
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
 
+require 'bundler/setup' unless ENV['COSMOS_DEVEL']
 require 'cosmos'
 require 'cosmos/tools/cmd_extractor/cmd_extractor'
 Cosmos::CmdExtractor.run
