e696df24af7eeeca668b904aa1cfc606980cc64b
spovilus <spovilus@lxlaptop>
2015-01-10 12:30:24 -0700

    fixed connection handeling, now can re-connect

--- TelemetryHandler.py
+++ TelemetryHandler.py
@@ -2,6 +2,7 @@ import threading
 import Queue
 import Globals
 import socket
+import errno
 
 TCP_IP = '192.168.1.141'
 TCP_PORT = 5005
@@ -28,23 +29,50 @@ class TelemetryHandler(threading.Thread):
         self.s.listen(1)
 
     def run(self):
+        while 1:
+            self.conn, addr = self.s.accept()
+            print 'Connection address:', addr
+            self.Connection()
+            print 'Closing connection from:', addr
+            #self.conn.close()
+        
+    def Connection(self):
         queues = { Globals.MOTOR_NOTIFICATION_OFFSET + 0:self.Motor1Queue,
                    Globals.MOTOR_NOTIFICATION_OFFSET + 1:self.Motor2Queue,
                    Globals.MOTOR_NOTIFICATION_OFFSET + 2:self.Motor3Queue,
                    Globals.IMU_NOTIFICATION_OFFSET + Globals.IMU_ID_ACC:self.AccQueue}
-        self.conn, addr = self.s.accept()
-        print 'Connection address:', addr
         while 1:
             queueNum = self.myNotificationQ.get()
             print "Got message from notification queue: " + str(queueNum)
             queues.setdefault(queueNum,self.DefaultMsg)
-            queues[queueNum]()
+            connClosed = queues[queueNum]()
+            if(connClosed):
+                break
 
             
     def Motor1Queue(self):
         msg = self.myMotor1Q.get()
         print "got message from motor 1: " + str(msg)
-        self.conn.send(msg)
+        try:  
+            self.conn.send(msg)
+        except socket.error, e:
+            if isinstance(e.args, tuple):
+                print "errno is %d" % e[0]
+                if e[0] == errno.EPIPE:
+                    # remote peer disconnected
+                    print "Detected remote disconnect"
+                else:
+                    # determine and handle different error
+                    pass
+            else:
+                print "socket error ", e
+            self.conn.close()
+            return True
+        except IOError, e:
+            # Hmmm, Can IOError actually be raised by the socket module?
+            print "Got IOError: ", e
+            return True
+        return False
         
     def Motor2Queue(self):
         msg = self.myMotor2Q.get()
