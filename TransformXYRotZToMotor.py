aa5d52d0f98f434b47c7f7a6d03fc038d6f3609b
spovilus <spovilus@lxlaptop>
2015-01-18 11:51:27 -0700

    fixing page number for source of transform

--- TransformXYRotZToMotor.py
+++ TransformXYRotZToMotor.py
@@ -1,6 +1,6 @@
 import math
 
-#from http://cdn.intechopen.com/pdfs-wm/8875.pdf pg 221
+#from http://cdn.intechopen.com/pdfs-wm/8875.pdf pg 211
 def TransformXYRotZToMotor(x,y,rotZ,d=1,debug = False):
     m1 = (-math.sin(math.pi/3)*x+math.cos(math.pi/3)*y+d*rotZ)/3
     m2 = (                                      -1.0*y+d*rotZ)/3
